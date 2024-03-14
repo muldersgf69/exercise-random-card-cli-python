c707f86d2b94f6df9b57e88762abb367380f437d
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2024-03-14 22:55:38 +0000

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import random
+
 suites = ['♡', '♢', '♤', '♧']
-values = list(range(1,14))
+numbers =['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q','K']
 
 def get_random_card():
-    # your code here
-    return "3♤"
-    
+    random_suite = random.choice(suites) #will pick a random suite for me
+    random_number = random.choice(numbers) #pick a random numbers from numbers
+    combine_random_suite_and_number = f"{random_number}{random_suite}"
 print(get_random_card())
